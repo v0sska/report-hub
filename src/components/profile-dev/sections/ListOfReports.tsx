6875fd9ab26c2608ce1d83c38cfbafff83cb9b33
Kosaniak21 <kosvik264@gmail.com>
2024-10-18 12:37:10 +0300

    Add sorted function to reports #2

--- src/components/profile-dev/sections/ListOfReports.tsx
+++ src/components/profile-dev/sections/ListOfReports.tsx
@@ -1,81 +1,88 @@
-import type React from 'react'
-import ReportCard from './ReportCard'
-import { useState, useEffect } from 'react'
-import reportService from '@/api/reportService'
+import reportService from "@/api/reportService";
+import { ReportType } from "@/components/types/interfaces/report/report-types";
+import type React from "react";
+import { useEffect, useState } from "react";
+import ReportCard from "./ReportCard";
 
 const ListOfReports: React.FC = () => {
-	const [reports, setReports] = useState<any[]>([]);
-	const [currentPage, setCurrentPage] = useState(1);
-	const reportsPerPage = 5; 
+  const [reports, setReports] = useState<ReportType[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const reportsPerPage = 5;
 
-	const fetchReports = async () => {
-		const data = await reportService.getByDeveloperId();
-		if (data) {
-			setReports(data);
-		}
-	}
+  const fetchReports = async () => {
+    const reports = await reportService.getByDeveloperId();
+    if (reports) {
+      const sortedReports = reports.sort((a, b) => {
+        const dateA = a.date ? new Date(a.date).getTime() : -Infinity;
+        const dateB = b.date ? new Date(b.date).getTime() : -Infinity;
+        return dateB - dateA;
+      });
 
-	useEffect(() => {
-		fetchReports();
-	}, []);
+      setReports(sortedReports);
+    }
+  };
 
-	const indexOfLastReport = currentPage * reportsPerPage;
-	const indexOfFirstReport = indexOfLastReport - reportsPerPage;
-	const currentReports = reports.slice(indexOfFirstReport, indexOfLastReport);
+  useEffect(() => {
+    fetchReports();
+  }, []);
 
-	const totalPages = Math.ceil(reports.length / reportsPerPage);
+  const indexOfLastReport = currentPage * reportsPerPage;
+  const indexOfFirstReport = indexOfLastReport - reportsPerPage;
+  const currentReports = reports.slice(indexOfFirstReport, indexOfLastReport);
 
-	const handleNextPage = () => {
-		if (currentPage < totalPages) {
-			setCurrentPage(currentPage + 1);
-		}
-	};
+  const totalPages = Math.ceil(reports.length / reportsPerPage);
 
-	const handlePrevPage = () => {
-		if (currentPage > 1) {
-			setCurrentPage(currentPage - 1);
-		}
-	};
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
-	return (
-		<div>
-			{reports.length > 0 ? (
-				<div className='flex flex-col gap-2'>
-					{currentReports.map(item => (
-						<ReportCard key={item.id} {...item} />
-					))}
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
 
-					<div className="flex justify-between items-center mt-4">
-						<div>
-							{currentPage > 1 && (
-								<button
-									onClick={handlePrevPage}
-									className='bg-gray-300 text-black px-4 py-2 rounded-lg'
-								>
-									Previous
-								</button>
-							)}
-						</div>
+  return (
+    <div>
+      {reports.length > 0 ? (
+        <div className="flex flex-col gap-2">
+          {currentReports.map((item) => (
+            <ReportCard key={item.id} {...item} />
+          ))}
 
-						<div>
-							{currentPage < totalPages && (
-								<button
-									onClick={handleNextPage}
-									className='bg-gray-300 text-black px-4 py-2 rounded-lg'
-								>
-									Next
-								</button>
-							)}
-						</div>
-					</div>
-				</div>
-			) : (
-				<div className='w-full text-center'>
-					You do not have any reports yet 💩
-				</div>
-			)}
-		</div>
-	)
-}
+          <div className="flex justify-between items-center mt-4">
+            <div>
+              {currentPage > 1 && (
+                <button
+                  onClick={handlePrevPage}
+                  className="bg-gray-300 text-black px-4 py-2 rounded-lg"
+                >
+                  Previous
+                </button>
+              )}
+            </div>
+
+            <div>
+              {currentPage < totalPages && (
+                <button
+                  onClick={handleNextPage}
+                  className="bg-gray-300 text-black px-4 py-2 rounded-lg"
+                >
+                  Next
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="w-full text-center">
+          You do not have any reports yet 💩
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default ListOfReports;
