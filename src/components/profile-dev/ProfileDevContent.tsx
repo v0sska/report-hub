6d38a29086a7920f7499a5438180c61dfdc79e25
Kosaniak21 <kosvik264@gmail.com>
2024-10-18 12:35:03 +0300

    Sort reports from latest to earliest

--- src/components/profile-dev/ProfileDevContent.tsx
+++ src/components/profile-dev/ProfileDevContent.tsx
@@ -1,38 +1,39 @@
-import type React from 'react'
-import Header from '../header/Header'
-import UserInfo from './sections/UserInfo'
-import AddReport from './sections/AddReport'
-import ListOfReports from './sections/ListOfReports'
-import type { ResDeveloperTypes } from '../types/interfaces/dev/res-dev'
-import { useEffect, useState } from 'react'
-import userService from '@/api/userService'
+import type React from "react";
+import Header from "../header/Header";
+import UserInfo from "./sections/UserInfo";
+import AddReport from "./sections/AddReport";
+import ListOfReports from "./sections/ListOfReports";
+import type { ResDeveloperTypes } from "../types/interfaces/dev/res-dev";
+import { useEffect, useState } from "react";
+import userService from "@/api/userService";
 
 const ProfileDevContent: React.FC = () => {
-	const [user, setUser] = useState<ResDeveloperTypes | null>(null)
-	useEffect(() => {
-		const fetchUser = async () => {
-			const data = await userService.getMy();
-			
-			const deveeloper = data.data.developer
-			if (deveeloper) {
-				setUser(deveeloper)
-			}
-		}
+  const [user, setUser] = useState<ResDeveloperTypes | null>(null);
 
-		fetchUser()
-	}, [])
-	return (
-		
-		<div className='flex flex-col gap-5'>
-			<Header name={user?.name || '...'} type={user?.stack || 'Developer'} />
-			<UserInfo
-				position={user?.stack || 'Developer'}
-				timeInJoinTeam={user?.timeJoin}
-			/>
-			<AddReport />
-			<ListOfReports />
-		</div>
-	)
-}
+  useEffect(() => {
+    const fetchUser = async () => {
+      const data = await userService.getMy();
 
-export default ProfileDevContent
+      const deveeloper = data.data.developer;
+      if (deveeloper) {
+        setUser(deveeloper);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  return (
+    <div className="flex flex-col gap-5">
+      <Header name={user?.name || "..."} type={user?.stack || "Developer"} />
+      <UserInfo
+        position={user?.stack || "Developer"}
+        timeInJoinTeam={user?.timeJoin}
+      />
+      <AddReport />
+      <ListOfReports />
+    </div>
+  );
+};
+
+export default ProfileDevContent;
--- src/components/types/interfaces/report/report-types.ts
+++ src/components/types/interfaces/report/report-types.ts
@@ -1,11 +1,11 @@
 export interface ReportType {
-	id?: string
-	created_at?: string
-	startWork: string
-	endWork: string
-	track: string
-	report: string
-	date?: string
-	customerId?: string
-	developerId?: string
+  id: string;
+  created_at: string;
+  startWork: string;
+  endWork: string;
+  track: string;
+  report: string;
+  date: string;
+  customerId: string;
+  developerId: string;
 }
